50f6fbe7421e38f2aef1b06a154ca65b2025b727
taoky <me@taoky.moe>
2023-02-11 19:51:36 +0800

    Add some routes to allow "lxc creation"

--- main.go
+++ main.go
@@ -12,7 +12,9 @@ import (
 	"math/big"
 	"net"
 	"net/http"
+	"strconv"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -56,6 +58,33 @@ func PVECheckAuth(c *gin.Context) error {
 		return errors.New("invalid CSRFPreventionToken")
 	}
 	return nil
+}
+
+type LXCResource struct {
+	Vmid   int    `json:"vmid"`
+	Mem    int    `json:"mem"`
+	Status string `json:"status"`
+	Name   string `json:"name"`
+	Uptime int    `json:"uptime"`
+	Lock   string `json:"lock"`
+	Node   string `json:"node"`
+	Maxmem int    `json:"maxmem"`
+}
+
+var containers = []LXCResource{}
+var containerMutex = &sync.Mutex{}
+
+func createWait(vmid int) {
+	// Mutex
+	time.Sleep(10 * time.Second)
+	containerMutex.Lock()
+	defer containerMutex.Unlock()
+	for i, container := range containers {
+		if container.Vmid == vmid {
+			fmt.Println("Unlocked container (mark as creation done)", vmid)
+			containers[i].Lock = ""
+		}
+	}
 }
 
 func mockPveServer(r *gin.Engine) error {
@@ -105,16 +134,159 @@ func mockPveServer(r *gin.Engine) error {
 		response = append(response, StorageContent{
 			Format: "tgz",
 			Size:   231060971,
-			Volid:  storage + ":vztmpl/debian-10-standard_10.7-1_amd64.tar.gz",
+			Volid:  storage + ":vztmpl/vlab-debian-10-standard_10.7-1_amd64.tar.gz",
 		})
 		response = append(response, StorageContent{
 			Format: "tgz",
 			Size:   243431756,
-			Volid:  storage + ":vztmpl/debian-11-standard_11.0-1_amd64.tar.gz",
+			Volid:  storage + ":vztmpl/vlab-debian-11-standard_11.0-1_amd64.tar.gz",
 		})
 		c.JSON(200, gin.H{
 			"data": response,
 		})
+	})
+	r.GET("/api2/json/cluster/resources", func(c *gin.Context) {
+		err := PVECheckAuth(c)
+		if err != nil {
+			// No ticket
+			c.Data(401, "", []byte(""))
+			return
+		}
+		type_ := c.Query("type")
+		if type_ != "vm" {
+			// not implemented
+			c.Data(400, "", []byte(""))
+			return
+		}
+		c.JSON(200, gin.H{
+			"data": containers,
+		})
+	})
+	r.POST("/api2/json/nodes/:node/lxc", func(c *gin.Context) {
+		err := PVECheckAuth(c)
+		if err != nil {
+			// No ticket
+			c.Data(401, "", []byte(""))
+			return
+		}
+		node := c.Param("node")
+		vmid, err := strconv.Atoi(c.PostForm("vmid"))
+		if err != nil {
+			fmt.Printf("invalid vmid: %v", err)
+			c.Data(400, "", []byte(""))
+			return
+		}
+		name := c.PostForm("name")
+
+		containerMutex.Lock()
+		defer containerMutex.Unlock()
+		for _, container := range containers {
+			if container.Vmid == vmid {
+				fmt.Printf("container %d exists", vmid)
+				c.Data(400, "", []byte(""))
+				return
+			}
+		}
+		containers = append(containers, LXCResource{
+			Vmid:   vmid,
+			Mem:    1024,
+			Maxmem: 1024000,
+			Status: "stopped",
+			Name:   name,
+			Uptime: 0,
+			Lock:   "create",
+			Node:   node,
+		})
+		go createWait(vmid)
+
+		upid := "UPID:" + node + ":00000000:00000000:00000000:vzcreate:" + strconv.Itoa(vmid) + ":mock@pve:"
+
+		c.JSON(200, gin.H{
+			"data": upid,
+		})
+	})
+	r.GET("/api2/json/nodes/:node/lxc/:vmid/config", func(c *gin.Context) {
+		err := PVECheckAuth(c)
+		if err != nil {
+			// No ticket
+			c.Data(401, "", []byte(""))
+			return
+		}
+		vmid := c.Param("vmid")
+		_ = c.Param("node")
+		vmidInt, err := strconv.Atoi(vmid)
+		if err != nil {
+			_ = fmt.Errorf("invalid vmid: %v", err)
+			c.Data(400, "", []byte(""))
+			return
+		}
+
+		containerMutex.Lock()
+		defer containerMutex.Unlock()
+		for _, container := range containers {
+			if container.Vmid == vmidInt {
+				c.JSON(200, gin.H{
+					"data": container,
+				})
+				return
+			}
+		}
+	})
+	r.GET("/api2/json/nodes/:node/tasks/:upid/status", func(c *gin.Context) {
+		err := PVECheckAuth(c)
+		if err != nil {
+			// No ticket
+			c.Data(401, "", []byte(""))
+			return
+		}
+		_ = c.Param("node")
+		upid := c.Param("upid")
+		upid_parts := strings.Split(upid, ":")
+		if len(upid_parts) != 9 {
+			fmt.Printf("invalid upid: %s\n", upid)
+			c.Data(400, "", []byte(""))
+			return
+		}
+		vmid, err := strconv.Atoi(upid_parts[6])
+		if err != nil {
+			fmt.Printf("invalid upid: %v\n", err)
+			c.Data(400, "", []byte(""))
+			return
+		}
+		event := upid_parts[5]
+		if err != nil {
+			fmt.Printf("invalid upid: %v\n", err)
+			c.Data(400, "", []byte(""))
+			return
+		}
+		fmt.Printf("vmid: %d, event: %s\n", vmid, event)
+		if event != "vzcreate" {
+			// not implemented
+			c.Data(400, "", []byte(""))
+			return
+		}
+		containerMutex.Lock()
+		defer containerMutex.Unlock()
+		for _, container := range containers {
+			if container.Vmid == vmid {
+				if container.Lock == "create" {
+					c.JSON(200, gin.H{
+						"data": gin.H{
+							"status": "running",
+						},
+					})
+					return
+				}
+				c.JSON(200, gin.H{
+					"data": gin.H{
+						"status":     "stopped",
+						"exitstatus": "OK",
+					},
+				})
+				return
+			}
+		}
+		c.Data(404, "", []byte(""))
 	})
 
 	cert, err := GenSelfSignedTLSCertificate()
@@ -131,8 +303,10 @@ func mockPveServer(r *gin.Engine) error {
 }
 
 func mockPostCreationServer(r *gin.Engine) error {
-	panic("not implemented")
-	// r.Run("127.0.0.1:8090")
+	r.POST("/set/:node/:vmid", func(c *gin.Context) {
+		c.Data(200, "", []byte(""))
+	})
+	return r.Run("127.0.0.1:8090")
 }
 
 func main() {
